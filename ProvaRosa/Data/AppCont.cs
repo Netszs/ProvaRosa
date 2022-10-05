bb2aa8efcdf337abece1d3355fe0e69db5a2fa69
Sebastião Neto <netiszs32@gmail.com>
2022-10-04 21:13:31 -0300

    Add project files.

---
+++ ProvaRosa/Data/AppCont.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaRosa.Models;
+
+namespace ProvaRosa.Data
+{
+    public class AppCont : DbContext
+    {
+        public AppCont(DbContextOptions<AppCont> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
